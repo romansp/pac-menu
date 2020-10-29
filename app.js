effab8001106461cf33f99beeb777035357c21a0
Roman Pavlov <rspaulau@gmail.com>
2020-10-28 21:53:26 -0700

    upgrade to vue 3

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-var apiUrl = 'https://conf.pac.by/menu/api/getMenu.php?k=1';
+const apiUrl = 'https://conf.pac.by/menu/api/getMenu.php?k=1';
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        loading: false,
-        menu: null
-    },
-    created: function () {
-        this.fetchData()
-    },
-    methods: {
-        fetchData: function () {
-            this.loading = true;
-            fetch(apiUrl)
-                .then(res => res.json())
-                .then(menu => {
-                    this.menu = menu;
-                    this.loading = false;
-                })
-                .catch(err => console.error(err));
-        }
+const app = Vue.createApp({
+  data() {
+    return {
+      loading: false,
+      menu: null,
     }
+  },
+  
+  async created() {
+    await this.fetchData();
+  },
+
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      try {
+        this.menu = await getMenu();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
 });
+
+const getMenu = async () => {
+  const res = await fetch(apiUrl);
+  return await res.json();
+}
+
+app.mount("#app");
